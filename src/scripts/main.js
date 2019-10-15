a420efcbc079afa9385fd45af0bb9b832f41c323
Sullivan Pierce <sullypierce@gmail.com>
2019-10-15 10:24:49 -0500

    finished cookie

---
+++ src/scripts/main.js
@@ -0,0 +1,7 @@
+const cookies = ["Oatmeal Raisin", "Chocolate Chip", "Sugar", "Peanut Butter", "Snickerdoodle", "Ginger"]
+
+
+for (let y = 1; y < cookies.length; y++) {
+    const currentCookie = cookies[y]
+    console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`)
+}
